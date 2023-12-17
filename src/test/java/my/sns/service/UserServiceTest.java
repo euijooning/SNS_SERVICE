654fdd1ac271f2736525ece945460aa7691a8cd5
EUIJOON <euijoonism@gmail.com>
2023-12-17 23:15:02 +0900

    [refactor] 테스트 코드 제대로 된 동작 위해 다시 롤백

--- src/test/java/my/sns/service/UserServiceTest.java
+++ src/test/java/my/sns/service/UserServiceTest.java
@@ -1,144 +1,103 @@
 package my.sns.service;
 
-import my.sns.dto.UserDto;
 import my.sns.exception.SnsApplicationException;
+import my.sns.fixture.UserEntityFixture;
 import my.sns.model.entity.UserEntity;
 import my.sns.repository.UserEntityRepository;
-import my.sns.util.JwtTokenUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
 class UserServiceTest {
-    @InjectMocks
+
+    @Autowired
     private UserService userService;
 
-    @Mock
+    @MockBean
     private UserEntityRepository userEntityRepository;
 
-    @Mock
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    @MockBean
+    private BCryptPasswordEncoder encoder;
 
-    private final String secretKey = "testSecretKey";
-    private final Long expiredMs = 3600000L;
+    @Test
+    @DisplayName("회원가입 성공 테스트")
+    void t1() {
+        String userName = "userName";
+        String password = "password";
 
-    public void setUp() {
-        MockitoAnnotations.initMocks(this);
-        ReflectionTestUtils.setField(userService, "secretKey", secretKey);
-        ReflectionTestUtils.setField(userService, "expiredMs", expiredMs);
+        UserEntity fixture = UserEntityFixture.get(userName, password);
+
+        when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.empty()); // 가입된 적이 없으므로 엠프티
+        when(encoder.encode(password)).thenReturn("encrypt password");
+        when(userEntityRepository.save(any())).thenReturn(UserEntityFixture.get(userName, password));
+
+        Assertions.assertDoesNotThrow(() -> userService.join(userName, password));
     }
 
     @Test
-    void testJoin() {
-        // given
-        String userName = "testUser";
-        String password = "testPassword";
-        when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.empty());
-        when(userEntityRepository.save(any())).thenAnswer(invocation -> invocation.getArgument(0));
+    @DisplayName("회원가입 실패 테스트 - 이미 동일한 userName으로 가입한 회원이 있는 경우")
+    void t2() {
+        String userName = "userName";
+        String password = "password";
 
-        // when
-        UserDto result = userService.join(userName, password);
+        UserEntity fixture = UserEntityFixture.get(userName, password);
 
-        // then
-        assertNotNull(result);
-        assertEquals(userName, result.getUserName());
+        when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.of(fixture));
+        when(userEntityRepository.save(any())).thenReturn(fixture); // save 메서드는 한 번만 호출
 
-        // verify that repository methods were called
-        verify(userEntityRepository, times(1)).findByUserName(userName);
-        verify(userEntityRepository, times(1)).save(any());
+        Assertions.assertThrows(SnsApplicationException.class, () -> userService.join(userName, password));
+    }
+
+
+    @Test
+    @DisplayName("로그인 성공 테스트")
+    void t3() {
+        String userName = "userName";
+        String password = "password";
+
+        UserEntity fixture = UserEntityFixture.get(userName, password);
+
+
+        when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.of(fixture));
+        when(encoder.matches(password, fixture.getPassword())).thenReturn(true);
+
+        Assertions.assertDoesNotThrow(() -> userService.login(userName, password));
     }
 
     @Test
-    void testJoin_DuplicateUserName() {
-        // given
-        String userName = "existingUser";
-        String password = "testPassword";
-        when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.of(mock(UserEntity.class)));
+    @DisplayName("로그인 실패 테스트 - userName으로 가입한 회원이 없는 경우")
+    void t4() {
+        String userName = "userName";
+        String password = "password";
 
-        // when, then
-        assertThrows(SnsApplicationException.class, () -> userService.join(userName, password));
-
-        // verify that repository method was called
-        verify(userEntityRepository, times(1)).findByUserName(userName);
-        // verify that save method was not called
-        verify(userEntityRepository, never()).save(any());
-    }
-
-    @Test
-    void testLogin() {
-        // given
-        String userName = "testUser";
-        String password = "testPassword";
-        UserEntity userEntity = new UserEntity();
-        userEntity.setPassword(bCryptPasswordEncoder.encode(password));
-        when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.of(userEntity));
-        when(bCryptPasswordEncoder.matches(password, userEntity.getPassword())).thenReturn(true);
-
-        // Mock JwtTokenUtils
-        JwtTokenUtils jwtTokenUtilsMock = mock(JwtTokenUtils.class);
-        when(jwtTokenUtilsMock.generateToken(eq(userName), anyString(), anyLong())).thenReturn("mockedToken");
-
-        // Inject the mocked JwtTokenUtils into the UserService
-        ReflectionTestUtils.setField(userService, "jwtTokenUtils", jwtTokenUtilsMock);
-
-        // when
-        String token = userService.login(userName, password);
-
-        // then
-        assertNotNull(token);
-        assertEquals("mockedToken", token);
-
-        // verify that repository method was called
-        verify(userEntityRepository, times(1)).findByUserName(userName);
-        // verify that matches method was called
-        verify(bCryptPasswordEncoder, times(1)).matches(password, userEntity.getPassword());
-        // verify that JwtTokenUtils.generateToken was called with non-null arguments
-        verify(jwtTokenUtilsMock, times(1)).generateToken(eq(userName), anyString(), anyLong());
-    }
-
-    @Test
-    void testLogin_InvalidPassword() {
-        // given
-        String userName = "testUser";
-        String password = "testPassword";
-        UserEntity userEntity = new UserEntity();
-        userEntity.setPassword(bCryptPasswordEncoder.encode("differentPassword"));
-        when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.of(userEntity));
-        when(bCryptPasswordEncoder.matches(password, userEntity.getPassword())).thenReturn(false);
-
-        // when, then
-        assertThrows(SnsApplicationException.class, () -> userService.login(userName, password));
-
-        // verify that repository method was called
-        verify(userEntityRepository, times(1)).findByUserName(userName);
-        // verify that matches method was called
-        verify(bCryptPasswordEncoder, times(1)).matches(password, userEntity.getPassword());
-    }
-
-    @Test
-    void testLogin_UserNotFound() {
-        // given
-        String userName = "nonExistingUser";
-        String password = "testPassword";
         when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.empty());
 
-        // when, then
-        assertThrows(SnsApplicationException.class, () -> userService.login(userName, password));
-
-        // verify that repository method was called
-        verify(userEntityRepository, times(1)).findByUserName(userName);
-        // verify that matches method was not called
-        verify(bCryptPasswordEncoder, never()).matches(any(), any());
+        Assertions.assertThrows(SnsApplicationException.class, () -> userService.login(userName, password));
     }
+
+    @Test
+    @DisplayName("로그인 실패 테스트 - userName으로 가입한 회원은 존재하지만, password를 틀린 경우")
+    void t5() {
+        String userName = "userName";
+        String password = "password";
+        String wrongPassword = "wrong";
+
+        UserEntity fixture = UserEntityFixture.get(userName, password);
+
+        when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.of(fixture));
+
+        Assertions.assertThrows(SnsApplicationException.class, () -> userService.login(userName, wrongPassword));
+    }
+
+
 }

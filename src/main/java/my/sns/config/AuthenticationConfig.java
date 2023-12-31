ffb15b8642f34fbbe5fc1c7e451870f7a6651281
EUIJOON <euijoonism@gmail.com>
2024-01-01 02:32:26 +0900

    [refactor] 토큰 헤더 관련 error 수정

--- src/main/java/my/sns/config/AuthenticationConfig.java
+++ src/main/java/my/sns/config/AuthenticationConfig.java
@@ -6,6 +6,7 @@ import my.sns.service.UserService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -21,6 +22,10 @@ public class AuthenticationConfig extends WebSecurityConfigurerAdapter {
     @Value("${jwt.secret-key}")
     private String key;
 
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().regexMatchers("^(?!/api/).*"); // 문자열의 정규화
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
